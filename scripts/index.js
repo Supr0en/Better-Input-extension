e96669d7f8a6c25e266b6922ccf08e71dee08978
Supr0en <erik.ruotsalainen1@gmail.com>
2023-05-22 10:56:48 +0300

    Working state

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,37 @@
+const position = (element, replacement) => {
+    var cursorStart = element.selectionStart;
+    var cursorEnd = element.selectionEnd;
+
+    var inputValue = element.value;
+
+    var textBeforeCursor = inputValue.substring(0, cursorStart);
+    var textAfterCursor = inputValue.substring(cursorEnd, inputValue.length);
+
+    element.value = `${textBeforeCursor}${replacement}${textAfterCursor}`;
+    element.setSelectionRange(cursorEnd + 1, cursorEnd + 1);
+}
+
 document.addEventListener('keydown', function(event) {
     var focusedInput = document.activeElement;
-
+    
     if (focusedInput) {
         if (window.getSelection().toString().length > 0) {
-            const highlightText = window.getSelection().toString();
+            var highlightedPart = window.getSelection().toString();
     
             switch (event.key) {
-                case '"': focusedInput.value = `"${highlightText}`; break;
-                case "'": focusedInput.value = `'${highlightText}`; break;
-                case ')': focusedInput.value = `(${highlightText}`; break;
-                case '}': focusedInput.value = `{${highlightText}`; break;
-                case ']': focusedInput.value = `[${highlightText}`; break;
+                case '"': position(focusedInput, `"${highlightedPart}`); break;
+                case "'": position(focusedInput, `'${highlightedPart}`); break;
+                case ')': position(focusedInput, `(${highlightedPart}`); break;
+                case '}': position(focusedInput, `{${highlightedPart}`); break;
+                case ']': position(focusedInput, `[${highlightedPart}`); break;
             }
         } else {
             switch (event.key) {
-                case '"': focusedInput.value = `"`; break;
-                case "'": focusedInput.value = `'`; break;
-                case ')': focusedInput.value = `(`; break;
-                case '}': focusedInput.value = `{`; break;
-                case ']': focusedInput.value = `[`; break;
+                case '"': position(focusedInput, `"`); break; 
+                case "'": position(focusedInput, `'`); break; 
+                case '(': position(focusedInput, `)`); break; 
+                case '{': position(focusedInput, `}`); break; 
+                case '[': position(focusedInput, `]`); break; 
             }
         }
     }
